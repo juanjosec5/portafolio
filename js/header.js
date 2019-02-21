a7c3b65bce94880bc48b97bff1b6018761541d7b
DESKTOP-38LCNL7\juanj <juan.josecuadros1@gmail.com>
2019-02-21 17:24:39 -0500

    navbar changes

--- js/header.js
+++ js/header.js
@@ -3,10 +3,12 @@ window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("header").style.height = "90px";
-        document.getElementById("header").style.background = "#fff";
+        document.getElementById("header").style.background = "#252525";
+
 
     } else {
         document.getElementById("header").style.height = "120px";
         document.getElementById("header").style.background = "none";
+
     }
 }
